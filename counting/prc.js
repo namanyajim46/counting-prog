c64eb40145956abecca2158f6c96e51263f4a1e2
Jim Prince <101920505+namanyajim46@users.noreply.github.com>
2022-12-25 16:03:43 +0300

    Add files via upload
    beginner counting prog with js

---
+++ counting/prc.js
@@ -0,0 +1,10 @@
+let countEl = document.getElementById("strt")
+console.log(countEl)
+let count = 0
+
+function start() {
+    count = count + 1
+    countEl.innerHTML = count
+
+}
+document.getElementById("header") = countingSeries;
